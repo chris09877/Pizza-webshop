eedba2d2d189fc9373eedd24371620ba7160c9eb
chris09877 <104131775+chris09877@users.noreply.github.com>
2024-01-10 09:40:11 +0100

    problem with patching orders resolved

--- pizza-house/src/Pizzas.jsx
+++ pizza-house/src/Pizzas.jsx
@@ -16,21 +16,20 @@ export default function Pizzas() {
     // let userId;
     const handleOrderClick = async () => {
         setShowBtn(!showBtn); // Toggles the showBtn state when the button is clicked
-        if (localStorage.getItem('userId') === null)
-        {
-        try {
-            console.log(`${config.apiUrl}}/orders/create`);
-            const response = await axios.post(`${config.apiUrl}/orders/create`);
-            const data = response.data;
-            setUserId(data.user);
-            console.log(`user id: ${userId}`);
-            console.log(`data id: ${data._id}`);
-            console.log(`user id from data: ${data.user}`);
-            localStorage.setItem('userId', data.user);
-        } catch (error) {
-            console.log(error);
+        if (localStorage.getItem('userId') === null) {
+            try {
+                console.log(`${config.apiUrl}}/orders/create`);
+                const response = await axios.post(`${config.apiUrl}/orders/create`);
+                const data = response.data;
+                setUserId(data.user);
+                console.log(`user id: ${userId}`);
+                console.log(`data id: ${data._id}`);
+                console.log(`user id from data: ${data.user}`);
+                localStorage.setItem('userId', data.user);
+            } catch (error) {
+                console.log(error);
+            }
         }
-    }
     };
 
     const handleSubmit = async (e) => {
@@ -42,11 +41,11 @@ export default function Pizzas() {
         formData.forEach((value, key) => {
             data[key] = value;
         });
-        data['user'] = userId;
+        data['user'] = localStorage.getItem('userId');
         console.log(`data in handlesubmit: ${data}`);
         try {
-            console.log(`${config.apiUrl}/orders/update/${userId}`);
-            const response = await axios.patch(`${config.apiUrl}/orders/update/${userId}`, data);
+            console.log(`${config.apiUrl}/orders/update/${localStorage.getItem('userId')}`);
+            const response = await axios.patch(`${config.apiUrl}/orders/update/${localStorage.getItem('userId')}`, data);
 
             //console.log(response.data); // Handle the response here (success message or other actions)
 
@@ -97,72 +96,45 @@ export default function Pizzas() {
                     title="Your Order"
 
                 />
-                {/* Other components */}
-                {/* ... */}
+
             </div>
             <h1>MENU</h1>
             {Array.isArray(pizzas) &&
-                // pizzas.map((pizza) => (
-                //     <form key={pizza._id} onSubmit={(e) => handleSubmit(e)}>
-                //         <div>
-                //             <Link to={`/pizzas/${pizza._id}`} >
 
-                //                 <h2>{pizza.name}</h2>
-                //                 <img src={pizza.imgUrl} alt={pizza.name} />
-                //                 <p>{pizza.price}</p>
-                //                 <p>{pizza.description}</p>
-                //                 <label>
-                //                     Quantity:
-                //                     <input
-                //                         type="number"
-                //                         name="quantity"
-                //                         defaultValue={1} // Default quantity is 1
-                //                         min={1}
-                //                     />
-                //                 </label>
-                //             </Link>
-                //             <button hidden={showBtn} type="submit">Add</button>
-
-                //         </div>
-                //         <input type="hidden" name="name" value={pizza.name} />
-                //         <input type="hidden" name="price" value={pizza.price} />
-                //         <input type="hidden" name="user" value={userId} />
-                //         <input type="hidden" name="pizza_id" value={pizza._id} />
-                //     </form>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-  {pizzas.map((pizza) => (
-    <form key={pizza._id} onSubmit={(e) => handleSubmit(e)}>
-      <div className="bg-white p-4 shadow-md">
-        <Link to={`/pizzas/${pizza._id}`} className="block">
-          <h2 className="text-lg font-semibold mb-2">{pizza.name}</h2>
-          <img src={`/${pizza.name}.jpg`} alt={pizza.name} className="w-full h-40 object-cover mb-2" />
-          <p className="text-gray-600 mb-2">{pizza.price}</p>
-          <p className="text-sm text-gray-500 mb-2">{pizza.description}</p>
-          
-        </Link>
-        <label className="block mb-2">
-            Quantity:
-            <input
-              type="number"
-              name="quantity"
-              defaultValue={1} // Default quantity is 1
-              min={1}
-              className="block w-full border border-gray-300 rounded px-2 py-1"
-            />
-          </label>
-        <button hidden={showBtn} type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-          Add
-        </button>
-        <input type="hidden" name="name" value={pizza.name} />
-        <input type="hidden" name="price" value={pizza.price} />
-        <input type="hidden" name="user" value={userId} />
-        <input type="hidden" name="pizza_id" value={pizza._id} />
-      </div>
-    </form>
-  ))}
-</div>
+                    {pizzas.map((pizza) => (
+                        <form key={pizza._id} onSubmit={(e) => handleSubmit(e)}>
+                            <div className="bg-white p-4 shadow-md">
+                                <Link to={`/pizzas/${pizza._id}`} className="block">
+                                    <h2 className="text-lg font-semibold mb-2">{pizza.name}</h2>
+                                    <img src={`/${pizza.name}.jpg`} alt={pizza.name} className="w-full h-40 object-cover mb-2" />
+                                    <p className="text-gray-600 mb-2">{pizza.price}</p>
+                                    <p className="text-sm text-gray-500 mb-2">{pizza.description}</p>
 
-  }
+                                </Link>
+                                <label className="block mb-2">
+                                    Quantity:
+                                    <input
+                                        type="number"
+                                        name="quantity"
+                                        defaultValue={1} // Default quantity is 1
+                                        min={1}
+                                        className="block w-full border border-gray-300 rounded px-2 py-1"
+                                    />
+                                </label>
+                                <button hidden={showBtn} type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                                    Add
+                                </button>
+                                <input type="hidden" name="name" value={pizza.name} />
+                                <input type="hidden" name="price" value={pizza.price} />
+                                <input type="hidden" name="user" value={userId} />
+                                <input type="hidden" name="pizza_id" value={pizza._id} />
+                            </div>
+                        </form>
+                    ))}
+                </div>
+
+            }
         </div>
     );
 };
